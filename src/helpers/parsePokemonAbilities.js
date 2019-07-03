d8c363f85f1fec840edfdf958c1b7fdc9aca2292
Sayan Sivakumaran <sivakusayan@gmail.com>
2019-07-03 19:07:57 -0400

    Added parsePokemonAbilities test

--- src/helpers/parsePokemonAbilities.js
+++ src/helpers/parsePokemonAbilities.js
@@ -2,16 +2,20 @@ import axios from 'axios';
 
 /**
  * Parses the data coming from the PokeAPI to get texts on 
- * a pokemon's abilities. Note that we need this function as
- * the text is not included in the initial data.
+ * a pokemon's abilities. Returns abilities in their correct order.
+ * Note that we need this function as the text is not included in the initial data.
  * 
  * @param {Object} data
  *  The data object coming from the PokeAPI
- * @returns {Promise<{name: string, effect: string, isHidden: boolean }>}
+ * @returns {Promise<[{name: string, effect: string, isHidden: boolean }]>}
  */
 export default (data) => {
-  const abilityLinks = data.abilities.map(ability => ability.ability.url);
-  const abilityIsHidden = data.abilities.map(ability => ability.is_hidden);
+  // Pokemon abilities need to come in a specific order, as some
+  // abilities are 'normal' while others are 'special'
+  const abilities = data.abilities.sort((a,b) => a.slot - b.slot);
+  
+  const abilityLinks = abilities.map(ability => ability.ability.url);
+  const abilityIsHidden = abilities.map(ability => ability.is_hidden);
   const abilityArray = abilityLinks.map((url, index) => axios.get(url)
     .then(res => {
       const data = res.data;
