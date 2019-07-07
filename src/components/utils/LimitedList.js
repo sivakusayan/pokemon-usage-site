0d39354f65d532f53a004b60caeca9a728184d5f
Sayan Sivakumaran <sivakusayan@gmail.com>
2019-07-07 17:47:31 -0400

    Wrote LimitedList component

--- src/components/utils/LimitedList.js
+++ src/components/utils/LimitedList.js
@@ -1,5 +1,49 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const LimitedList = () => null;
+class LimitedList extends React.Component {
+  state = {
+    limit: this.props.limit,
+    increment: this.props.increment,
+  }
+
+  increment = () => {
+    this.setState((prevState) => ({
+      limit: prevState.limit + prevState.increment 
+    }));
+  }
+
+  render = () => {
+    const { children } = this.props;
+    const { limit } = this.state;
+    return (
+      <div className='limited-list'>
+        <ul className='limited-list__list'>
+          {children.slice(limit)}
+        </ul>
+        {children.length > limit && (
+          <button 
+            onClick={this.increment} 
+            className='limited-list__increment'
+          >
+            Show More
+          </button>
+        )}
+      </div>
+    )
+  }
+}
+
+LimitedList.propTypes = {
+  limit: PropTypes.number,
+  increment: PropTypes.number,
+  children: PropTypes.arrayOf(PropTypes.node),
+}
+
+LimitedList.defaultProps  = {
+  limit: 10,
+  increment: 10,
+  children: [],
+}
 
 export default LimitedList;
--- src/components/utils/__tests__/LimitedList.js
+++ src/components/utils/__tests__/LimitedList.js
@@ -2,33 +2,37 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import LimitedList from '../LimitedList';
 
-it('renders no more list items than the limit', () => {
+let children;
+
+beforeAll(() => {
   const list = ['apple', 'fruit', 'banana', 'strawberry', 'pear'];
+  children = list.map(word => <li>{word}</li>);
+})
+
+it('renders no more list items than the limit', () => {
   const limit = 3;
-  const wrapper = shallow(<LimitedList list={list} limit={limit} />);
+  const wrapper = shallow(<LimitedList children={children} limit={limit} />);
 
   expect(wrapper.find('ul.limited-list__list').children.length).toBeLessThanOrEqual(limit);
 });
-it('increment button doesn\'t show when there are less child nodes than the limit', () => {
-  const list = ['apple', 'fruit', 'banana', 'strawberry', 'pear'];
-  const limit = 8;
-  const wrapper = shallow(<LimitedList list={list} limit={limit} />);
+it('increment button doesn\'t show when there are limit is greater than or equal to children length', () => {
+  const limit = 5;
+  const wrapper = shallow(<LimitedList children={children} limit={limit} />);
 
   expect(wrapper.find('button.limited-list__increment').exists()).toBeFalsy();
 });
 it('increment button does show when there are more child nodes than the limit', () => {
-  const list = ['apple', 'fruit', 'banana', 'strawberry', 'pear'];
   const limit = 3;
-  const wrapper = shallow(<LimitedList list={list} limit={limit} />);
+  const wrapper = shallow(<LimitedList children={children} limit={limit} />);
 
   expect(wrapper.find('button.limited-list__increment').exists()).toBeTruthy();
 });
 it('increases the limit when the increment button is clicked', () => {
-  const limit = 5;
+  const limit = 3;
   const increment = 5;
-  const wrapper = shallow(<LimitedList limit={limit} increment={increment} />);
+  const wrapper = shallow(<LimitedList children={children} limit={limit} increment={increment} />);
 
   wrapper.find('button.limited-list__increment').simulate('click');
   
-  expect(wrapper.state('limit')).toBeEqual(limit + increment);
+  expect(wrapper.state('limit')).toEqual(limit + increment);
 });
