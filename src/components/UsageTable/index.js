b60471fe28f28f86b4d9b7f3d18bc0dfa3127074
Sayan Sivakumaran <sivakusayan@gmail.com>
2019-07-07 16:30:53 -0400

    Prepping app for development

---
+++ src/components/UsageTable/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const PokemonList = () => null;
+
+export default PokemonList;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/index.css';
+import './styles/index.scss';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-import fetchPokemonOverview from './helpers/fetchPokemonOverview.js';
+ReactDOM.render(<App />, document.getElementById('root'));
 
-fetchPokemonOverview('Ditto').then(res => console.log(res));
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
